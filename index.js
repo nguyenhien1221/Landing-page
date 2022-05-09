d97e063dbe61eb995349c7ae0523d1f373af0ef3
NguyenHien <nguyenhien2942000@gmail.com>
2022-05-10 00:15:08 +0700

    push code 10/5/2022

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.padding = "10px 0px";
+    document.getElementById("navbar").style.boxShadow = "0px 2px 5px #9a9a9a"
+    
+  } else {
+    document.getElementById("navbar").style.padding = "20px 0px";
+    document.getElementById("navbar").style.boxShadow = "unset"
+  }
+}
